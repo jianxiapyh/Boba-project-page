717df766a69c9bc8239b5970a15130bde5396b8c
Yihan P <yihanp2@illinois.edu>
2026-08-17 21:07:20 -0500

    Move package interaction to end

--- scripts/check_site.py
+++ scripts/check_site.py
@@ -36,6 +36,7 @@ FIDELITY_CASES = {
     "single_clift_cloth_1": 80,
     "single_clift_cloth_3": 97,
     "double_lift_cloth_1": 116,
+    "weird_package": 39,
 }
 EXCLUDED_SUPPLEMENTAL_CASES = {
     "cloth_blue_fold",
